534b9c95af062c7a3ae6689d0d9cdeb9a7ecc3af
Noixelfer <pauldarius98@gmail.com>
2021-09-19 15:35:11 +0300

    Feat: Fixes

---
+++ Assets/Scripts/Runtime/UI/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+	[SerializeField] private Button playButton;
+
+	private void Awake()
+	{
+		playButton.onClick.AddListener(Play);
+	}
+
+	private void Play()
+	{
+		SceneManager.LoadScene("EnvironmentScene");
+	}
+}
